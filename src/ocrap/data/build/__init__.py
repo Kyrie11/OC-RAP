d728003d9d4cbf133890e06e787d452c7a0e9ddf
Kyrie11 <770790858@qq.com>
2026-07-25 17:04:06 +0800

    Revert "支持数据集生成中断写manifest.csv"
    This reverts commit 3ab503af472340cc9cbbba9d68b841772d89b5e0.
    
    rever befor v48.2:

--- src/ocrap/data/build/__init__.py
+++ src/ocrap/data/build/__init__.py
@@ -1,5 +1,3 @@
 from .builder import build_dataset, build_samples_for_history, select_planning_times
 from .diagnose import diagnose_dataset
 from .papercheck import papercheck_dataset
-
-from .manifest import rebuild_manifest
